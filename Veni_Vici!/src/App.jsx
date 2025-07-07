885ecd99f083afedca7cc6aedca31059339661ce
Edward Torres <etorres5124@gmail.com>
2025-07-06 21:22:13 -0700

    button for three attributes

--- Veni_Vici!/src/App.jsx
+++ Veni_Vici!/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [currentImage, setCurrentImage] = useState(null);
   const [prevImages, setPrevImages] = useState([]);
   const [banImages, setBanImages] = useState([]);
-  const [catInfo, setCatInfo]= useState({})
+  const [catInfo, setCatInfo]= useState({life_span:"", origin: "", weight:""})
  
 
 // Function to fetch data from the API
@@ -23,7 +23,9 @@ function App() {
       const json = await response.json();
       const data = json[0];
       console.log(json)
-      console.log(json[0].breeds[0])
+      console.log(json[0].breeds[0].life_span)
+      console.log(json[0].breeds[0].origin)
+      console.log(json[0].breeds[0].weight.metric)
 
       console.log("url:"+json[0].url);
       console.log("name:"+json[0].name);
@@ -33,6 +35,7 @@ function App() {
       } else {
         setCurrentImage(json[0].url);
         setPrevImages((images) => [...images, json[0].url]);
+        setCatInfo({life_span: json[0].breeds[0].life_span, origin: json[0].breeds[0].origin, weight:json[0].breeds[0].weight.metric });
       }
     } catch (error) {
       console.error("Error fetching data from API:", error);
@@ -62,6 +65,9 @@ function App() {
       ) : (
         <div> </div>
       )}
+      <button>{catInfo.life_span}</button>
+      <button>{catInfo.origin}</button>
+      <button>{catInfo.weight}</button>
 
       <button type='submit' className='button' onClick={makeQuery}>Show that cat!🎞</button>
     
